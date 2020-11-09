dfcea0a99bb65b32dfb12889109ddd4cd4430a00
Mohammed Nabeel km <nabeelkmvkd07@gmail.com>
2020-11-09 16:20:11 +0530

    MongoDB atlas DB is connected with expressjs

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,15 @@ const port  = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri , {useNewUrlParser : true , useCreateIndex: true , useUnifiedTopology: true});
+
+const connection = mongoose.connection;
+
+connection.once('open' , () => {
+    console.log("MongoDB databaase onnection estbsished successfully")
+})
+
 app.listen(port , () => {
     console.log(`server is running on port: ${port} `)
 });
