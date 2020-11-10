744fbe3524cacaec47e5c0e40a0ee054878e845a
Mohammed Nabeel km <nabeelkmvkd07@gmail.com>
2020-11-10 14:07:47 +0530

    Connecting DB user and exercise with ui forms using axios api fetch

--- src/components/CreateUser.js
+++ src/components/CreateUser.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class CreateUser extends Component {
 
@@ -27,6 +28,9 @@ class CreateUser extends Component {
             username : this.state.username
         }
         console.log(user);
+
+        axios.post('http://localhost:5000/users/add' , user)
+        .then(res => console.log(res.data));
 
 
         this.setState({
